001bd2909b81b9ff5bb08485b64eaa1dcd72e1ff
Serge Pomorski <spomorski@google.com>
2017-06-06 14:38:22 -0400

    Update sample projects for Bazel tutorials. (#23)
    * Add Java companion project for the Java build tutorial.
    
    * Update the iOS sample project to match cl/157118248.
    
    * Add sample C++ project for the C++ build tutorial.

---
+++ cpp-tutorial/stage3/main/hello-world.cc
@@ -0,0 +1,14 @@
+#include "lib/hello-time.h"
+#include "main/hello-greet.h"
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv) {
+  std::string who = "world";
+  if (argc > 1) {
+    who = argv[1];
+  }
+  std::cout << get_greet(who) <<std::endl;
+  print_localtime();
+  return 0;
+}
---
+++ java-tutorial/src/main/java/com/example/Greeting.java
@@ -0,0 +1,8 @@
+package com.example;
+
+    public class Greeting {
+        public static void sayHi() {
+            System.out.println("Hi!");
+    }
+}
+

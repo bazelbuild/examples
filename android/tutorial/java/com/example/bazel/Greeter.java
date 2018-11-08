24b49c521e090e94fc4e9bf40518482a989c5c5b
Jingwen Chen <jingwen@google.com>
2018-11-08 15:25:22 -0500

    More interactive example

---
+++ android/tutorial/java/com/example/bazel/Greeter.java
@@ -0,0 +1,13 @@
+package com.example.bazel;
+
+/**
+ * A tiny Greeter library for the Bazel Android "Hello, World" app.
+ */
+public class Greeter {
+
+  public Greeter(){ }
+
+  public String sayHello() {
+    return "Hello Bazel! \uD83D\uDC4B";
+  }
+}
--- android/tutorial/java/com/example/bazel/MainActivity.java
+++ android/tutorial/java/com/example/bazel/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.bazel;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Main class for the Bazel Android "Hello, World" app.
@@ -11,8 +13,16 @@ public class MainActivity extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     Log.v("Bazel", "Hello, Android");
-    Log.v("Bazel", "Lib says: " + Lib.message());
+
+    setContentView(R.layout.activity_main);
+
+    Button clickMeButton = findViewById(R.id.clickMeButton);
+    TextView helloBazelTextView = findViewById(R.id.helloBazelTextView);
+
+    Greeter greeter = new Greeter();
+
+    // Bazel supports Java 8 language features like lambdas!
+    clickMeButton.setOnClickListener(v -> helloBazelTextView.setText(greeter.sayHello()));
   }
 }

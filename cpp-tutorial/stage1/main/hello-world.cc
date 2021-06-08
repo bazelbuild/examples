6af8508ccc4c0cc3d8c0ac2e4ec2851476a761a4
marvinwener <45437696+marvinwener@users.noreply.github.com>
2021-06-08 11:31:58 +0200

    Update hello-world.cc

--- cpp-tutorial/stage1/main/hello-world.cc
+++ cpp-tutorial/stage1/main/hello-world.cc
@@ -4,7 +4,7 @@
 
 std::string get_greet(const std::string& who) {
   return "Hello " + who + " from TIOBE";
-  int varThatHasNoPurpose = 1/0;
+  int varThatHasNoPurpose;
 }
 
 void print_localtime() {

813d783e68ab0e5ced85fc545e22079dcd22ed53
Abhilash Gnan <abhilashgnan@gmail.com>
2021-07-27 00:12:00 +0200

    Fix indents in java-tutorial (#196)

--- java-tutorial/src/main/java/com/example/Greeting.java
+++ java-tutorial/src/main/java/com/example/Greeting.java
@@ -2,7 +2,6 @@ package com.example;
 
 public class Greeting {
     public static void sayHi() {
-            System.out.println("Hi!");
+        System.out.println("Hi!");
     }
 }
-
--- java-tutorial/src/main/java/com/example/cmdline/Runner.java
+++ java-tutorial/src/main/java/com/example/cmdline/Runner.java
@@ -2,8 +2,8 @@ package com.example.cmdline;
 
 import com.example.Greeting;
 
-   public class Runner {
-       public static void main(String args[]) {
-           Greeting.sayHi();
-   }
+public class Runner {
+    public static void main(String args[]) {
+        Greeting.sayHi();
+    }
 }

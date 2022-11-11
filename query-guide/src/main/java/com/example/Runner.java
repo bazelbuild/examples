cb17d11c7a5b6d5c0f43566975d0a1f1ef837a6c
lsnose <lnose@google.com>
2022-11-11 15:28:22 -0800

    Revert "Revert "Adding query guide to examples""
    This reverts commit 7a96bd3769e1bf114f066cdd9f45462d33bf6314.

---
+++ query-guide/src/main/java/com/example/Runner.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.example.restaurant.Cafe;
+
+public class Runner {
+    public static void main(String[] args) {
+        Cafe cafeBazel = new Cafe();
+        cafeBazel.printDishes();
+    }
+}

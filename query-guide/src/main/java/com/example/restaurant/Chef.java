a151550f302f45fa0e552a4c43586e1989aad9e1
lsnose <lnose@google.com>
2022-11-11 16:33:50 -0800

    Adding newline at eof

--- query-guide/src/main/java/com/example/restaurant/Chef.java
+++ query-guide/src/main/java/com/example/restaurant/Chef.java
@@ -5,13 +5,12 @@ import com.example.dishes.Pizza;
 import java.util.HashMap;
 
 public class Chef {
-    
-    public HashMap<String,String> listDishes() {
-        HashMap<String,String> dishes = new HashMap<String,String>();
 
-        dishes.put(MacAndCheese.DISH_NAME,MacAndCheese.DESCRIPTION);
-        dishes.put(Pizza.DISH_NAME,Pizza.DESCRIPTION);
+    public HashMap<String, String> listDishes() {
+        HashMap<String, String> dishes = new HashMap<String, String>();
 
+        dishes.put(MacAndCheese.DISH_NAME, MacAndCheese.DESCRIPTION);
+        dishes.put(Pizza.DISH_NAME, Pizza.DESCRIPTION);
 
         return dishes;
     }
--- query-guide/src/main/java/com/example/reviews/Review.java
+++ query-guide/src/main/java/com/example/reviews/Review.java
@@ -1,7 +1,7 @@
 package com.example.reviews;
 
 public class Review {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         System.out.println("My dish was delicious! (5/5)");
     }
 }

8555f3f3dc9cf548528700300bc807a6280fd7da
lsnose <lnose@google.com>
2022-11-17 11:56:16 -0800

    Changing name of folder to query-quickstart

---
+++ query-quickstart/src/main/java/com/example/dishes/Pizza.java
@@ -0,0 +1,6 @@
+package com.example.dishes;
+
+public class Pizza {
+    public static final String DISH_NAME = "Pizza";
+    public static final String DESCRIPTION = "Cheesy Delicious Goodness";
+}
---
+++ query-quickstart/src/main/java/com/example/restaurant/Chef.java
@@ -0,0 +1,17 @@
+package com.example.restaurant;
+
+import com.example.dishes.MacAndCheese;
+import com.example.dishes.Pizza;
+import java.util.HashMap;
+
+public class Chef {
+
+    public HashMap<String, String> listDishes() {
+        HashMap<String, String> dishes = new HashMap<String, String>();
+
+        dishes.put(MacAndCheese.DISH_NAME, MacAndCheese.DESCRIPTION);
+        dishes.put(Pizza.DISH_NAME, Pizza.DESCRIPTION);
+
+        return dishes;
+    }
+}

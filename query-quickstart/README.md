2c90149627270167b0433ae8dd0dbc7e94f52c6e
Lillian Nose <lnose@google.com>
2022-11-23 19:56:26 +0000

    Adding query quickstart example (#266)
    * Adding query guide to examples
    
    * Revert "Adding query guide to examples"
    
    This reverts commit 2a26e8e216822a463f755b56c716353a354afb3b.
    
    * Revert "Revert "Adding query guide to examples""
    
    This reverts commit 7a96bd3769e1bf114f066cdd9f45462d33bf6314.
    
    * Removing .DS_Store files
    
    * Adding newline at eof
    
    * Changing name of folder to query-quickstart
    
    * Changing directory name and updating tutorials.yml
    
    * Changing name in tutorial.yml
    
    * Adding README
    
    * Adding title to README

---
+++ query-quickstart/README.md
@@ -0,0 +1,6 @@
+Query Quickstart
+----------------
+
+This project runs you through a set of basic queries you can use to learn more about your project's file dependencies. It is intended for new Bazel developers with a basic knowledge of how Bazel and `BUILD` files work.
+
+Read the [Bazel quickstart](https://bazel.build/query/quickstart) tutorial to follow along.
---
+++ query-quickstart/src/main/java/com/example/restaurant/Cafe.java
@@ -0,0 +1,17 @@
+package com.example.restaurant;
+
+import java.util.HashMap;
+
+public class Cafe {
+
+    public void printDishes() {
+        Chef chef = new Chef();
+        HashMap<String, String> dishes = chef.listDishes();
+
+        System.out.println("-------------- MENU ---------------");
+        for (String dishName : dishes.keySet()) {
+            System.out.println(dishName + " - " + dishes.get(dishName));
+        }
+        System.out.println("-----------------------------------");
+    }
+}

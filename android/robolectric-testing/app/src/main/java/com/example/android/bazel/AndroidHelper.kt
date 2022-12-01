6de945b2de6d4081bc8b6082ff4928e8e96df989
Benjamin Lee <ben@ben.cm>
2022-12-01 12:03:36 -0800

    Add Robolectric example

--- android/robolectric-testing/app/src/main/java/com/example/android/bazel/AndroidHelper.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android.bazel
-
-object AndroidHelper {
-    fun add(one: Int, two: Int): Int {
-        return one + two
-    }
-}
---
+++ android/robolectric-testing/app/src/main/java/com/example/android/bazel/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.bazel
+
+import android.app.Activity
+import android.os.Bundle
+
+class LoginActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

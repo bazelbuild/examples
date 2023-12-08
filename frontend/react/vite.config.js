5179075c8290f0d038c2532e1c88e5ce3bd9a943
Alex Eagle <alex@aspect.dev>
2023-12-07 16:36:58 -0800

    cleanup: simplify config, this was the default

--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -5,10 +5,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [svgr({
-    // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
-    include: "**/*.svg?react"
-  }), react()],
+  plugins: [svgr(), react()],
   test: {
     globals: true,
     css: true,

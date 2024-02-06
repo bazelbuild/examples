ee9c67c560e4289df587ecfac88b040923321b0c
Vinh Tran <vinhdaitran@google.com>
2024-02-05 23:49:23 -0500

    Simply cc program to not require libcxx

--- cc-toolchain/main/hello-world.cc
+++ cc-toolchain/main/hello-world.cc
@@ -1,12 +1,6 @@
-#include <iostream>
-
-std::string get_greet(const std::string& who) {
-  return "Hello " + who;
-}
-
-
-int main(int argc, char** argv) {
-//   std::string who = "world";
-//   std::cout << get_greet(who) << std::endl;
-  return 0;
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }

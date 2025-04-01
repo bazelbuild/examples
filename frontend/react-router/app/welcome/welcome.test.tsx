67660aac03ae478e043a8d839d885d44a3884d2f
Alex Eagle <alex@aspect.dev>
2025-03-31 17:59:57 -0700

    chore: upgrade @testing-library/jest-dom

--- frontend/react-router/app/welcome/welcome.test.tsx
+++ frontend/react-router/app/welcome/welcome.test.tsx
@@ -1,6 +1,6 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/vitest';
 
 import { render, screen } from '@testing-library/react';
 import { Welcome } from "./welcome";

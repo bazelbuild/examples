3a83e424e666249dc884b22be129eca7d897a722
Yun Peng <pcloudy@google.com>
2025-12-17 15:18:33 +0000

    Restore override examples with abseil-cpp and re2
    - Use abseil-cpp for archive_override
    - Use re2 for git_override
    - This restores the override examples while ensuring compatibility with Bazel 9

--- bzlmod/02-override_bazel_module/main.cc
+++ bzlmod/02-override_bazel_module/main.cc
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "lib_a.h"
+#include "re2/re2.h"
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello from the main module!" << std::endl;
     lib_a();
+
+    if (RE2::FullMatch("bazel", "b.*l")) {
+        std::cout << "RE2 match!" << std::endl;
+    }
 }
